f80239d461a17bf282b24df98ac0db0a1fdd5b61
Nathan Braswell <miloignis@gmail.com>
2017-03-29 12:31:27 -0400

    start work on ubuntu provisioning script for VMs, etc

---
+++ provision_ubuntu.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#sudo apt-get update
+#sudo apt-get upgrade
+#sudo apt-get install vim git
+#git clone https://github.com/Limvot/vim_config.git
+cd vim_config
+git submodule init
+git submodule update
+#./setup.sh
+
